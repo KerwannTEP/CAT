6388d82c1917371146ccf746d2f5936dba5f7427
K <kerwann_tep@yahoo.fr>
2020-12-26 13:08:25 +0100

    poly3 real solver

--- code/sources/Mean.jl
+++ code/sources/Mean.jl
@@ -74,3 +74,29 @@ end
 function tangentVelocity(E::Float64, L::Float64, r::Float64)
     return L/r
 end
+
+##################################################
+# Solve the degree-3 polynomial equation aX^3 + bX^2 + cX + d = 0
+# Gives the real roots
+##################################################
+
+function solveRealPoly3(a::Float64, b::Float64, c::Float64, d::Float64)
+    p = (3*a*c - b^2)/(3*a^2)
+    q = (2*b^3 - 9*a*b*c + 27*a^2*d)/(27*a^3)
+    disc = -(4*p^3 + 27*q^2)
+    if (disc < 0.0)
+        rt1 = cbrt(-q/2 + sqrt(q^2/4 + p^3/27)) + cbrt(-q/2 - sqrt(q^2/4 + p^3/27))
+        rt1 -= b/(3*a)
+        nbRoot = 1
+        return nbRoot, rt1
+    else
+        rt1 = 2*sqrt(-p/3)*((1/3)*acos((3*q)/(2*p) * sqrt(-3/p)))
+        rt2 = 2*sqrt(-p/3)*((1/3)*acos((3*q)/(2*p) * sqrt(-3/p)) - 2*pi/3)
+        rt3 = 2*sqrt(-p/3)*((1/3)*acos((3*q)/(2*p) * sqrt(-3/p)) - 4*pi/3) 
+        rt1 -= b/(3*a)
+        rt2 -= b/(3*a)
+        rt3 -= b/(3*a)
+        nbRoot = 3
+        return nbRoot, rt1, rt2, rt3
+    end
+end
