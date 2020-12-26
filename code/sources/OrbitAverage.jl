f5c6beaedb355920364e0826f58b1008e248256e
K <kerwann_tep@yahoo.fr>
2020-12-26 13:18:54 +0100

    new radiusBound

--- code/sources/OrbitAverage.jl
+++ code/sources/OrbitAverage.jl
@@ -45,50 +45,25 @@ function discriminantY(p::Float64, q::Float64)
     return -(4.0*p^3+27*q^2)
 end
 
-# Returns a tuple (number of roots, root1, ...) of the roots of the X-polynomial
-# Returns (0,"Unbounded orbit") if E <= 0
-function rootX(E::Float64, L::Float64)
-    if (E <= 0.0)
-        return (0,"Unbounded orbit")
-    else
-        a = _alpha(E,L)
-        b = _beta(E,L)
-        p = _p(E,L)
-        q = _q(E,L)
-        disc = discriminantY(p,q)
-        if (disc < 0.0)
-            rt = cbrt(-q/2+sqrt(q^2/4+p^3/27)) + cbrt(-q/2-sqrt(q^2/4+p^3/27))
-            rt += b/(3.0*a)
-            return (1,rt)
-        else
-            rt1 = 2*sqrt(-p/3.0)*cos(1.0/3 * acos(3.0*q/(2.0*p) * sqrt(-3.0/p)))
-            rt1 -= b/(3.0*a)
-            rt2 = 2*sqrt(-p/3.0)*cos(1.0/3 * acos(3.0*q/(2.0*p) * sqrt(-3.0/p))-2.0*pi/3)
-            rt2 -= b/(3.0*a)
-            rt3 = 2*sqrt(-p/3.0)*cos(1.0/3 * acos(3.0*q/(2.0*p) * sqrt(-3.0/p))-2.0*pi*2.0/3)
-            rt3 -= b/(3.0*a)
-            return (3, [rt1, rt2, rt3])
-        end
-    end
-end
-
 # Returns "Unbounded orbit" if E<=0 and (rmin,rmax) if the parameters are allowed
 # Check beforehands that the parameters are allowed, i.e!
 # -> E,L are positive
 # -> E < Ec(L)
 function radiusBounds(E::Float64, L::Float64)
-    nbRoots, rts = rootX(E,L)
-    if (nbRoots == 0)
-        return rts
+    if (E <= 0.0)
+        return (0,"Unbounded orbit")
     else
-        disc = discriminantY(E,L)
-        # Check if the parameters (E,L) can describe an orbit
-        @assert nbRoots == 3 "radiusBounds: Forbidden parameters (E,L)"
-    
-        rts = sort(rts)  # Sort the roots
-        rmin, rmax = rts[2], rts[3] # Lowest root is regative which the two other are positive
-        rmin = sqrt(rmin)
-        rmax = sqrt(rmax)    
+        a = 4.0*E^2
+        b = 4.0*(E^2-1.0+E*L^2)
+        c = 4.0*E*L^2+L^4
+        d = L^4
+        nbRoot, rts = solveRealPoly3(a,b,c,d)
+
+        @assert (nbRoot == 3) "radiusBounds: Forbidden parameters (E,L)"       
+        rts = collect(rts)
+        rts = sort(rts)
+        rmin = sqrt(rts[2])
+        rmax = sqrt(rts[3])
         return rmin, rmax
     end
 end
