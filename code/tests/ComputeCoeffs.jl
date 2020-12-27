e02ea7bc60ffe3236d89b488fde2cd88a4cfb3f0
K <kerwann_tep@yahoo.fr>
2020-12-27 22:30:39 +0100

    Compute diffusion coefficients

---
+++ code/tests/ComputeCoeffs.jl
@@ -0,0 +1,13 @@
+include("../sources/Main.jl")
+
+E = 0.1
+L = 0.1
+q = 0.0
+m = 0.1
+
+@time dE, dE2, dL, dL2, dEdL = averageDiffCoeffs(E,L,q,m)
+println("D_E  =  ",dE)
+println("D_EE =  ",dE2)
+println("D_L  =  ",dL)
+println("D_LL =  ",dL2)
+println("D_EL =  ",dEdL)
