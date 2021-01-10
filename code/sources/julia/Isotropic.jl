c2600f4dcc0ea6bce6dbf1f2b81b7fd3c66f5415
KerwannTEP <kerwann_tep@yahoo.fr>
2021-01-10 14:29:13 +0100

    orbit average iso

--- code/sources/julia/Isotropic.jl
+++ code/sources/julia/Isotropic.jl
@@ -73,3 +73,97 @@ function localOrbitChangeIso!(r::Float64, E::Float64, L::Float64, m_field::Float
 
     end
 end
+
+
+function averageDiffCoeffsIso!(E::Float64, L::Float64, m_field::Float64,
+                               PlummerTable::IntTable = PlummerTable_serial)
+
+    @assert (E>0.0 && L>0.0) "averageDiffCoeffs: E and L must be non-negative"
+
+    IntTable_init!(PlummerTable)
+
+    local halfperiod, rmin, rmax, rc, r1, r2, th1, th2, nbT1, nbT2, nbr,
+          dE, dE2, dL, dL2, dEdL
+    let halfperiod, rmin, rmax, rc, r1, r2, th1, th2, nbT1, nbT2, nbr,
+        dE, dE2, dL, dL2, dEdL
+
+    halfperiod = 0.0
+    rmin, rmax = radiusBounds(E,L)
+    rc = maximizerPsiEff(L)
+    r1, r2 = (rmin+rc)/2, (rc+rmax)/2
+    th1, th2 = asin(sqrt(psiEff(r1,L)-E)), asin(sqrt(psiEff(r2,L)-E))   
+
+    nbT1 = 20
+    nbT2 = 20
+    nbr = 30
+
+    dth1 = th1/nbT1
+    dth2 = th2/nbT2
+    dr = (r2-r1)/nbr
+    dE, dE2, dL, dL2, dEdL = 0.0, 0.0, 0.0, 0.0, 0.0
+
+    # Orbit-average and computation of halfperiod
+    for iTh=1:nbT1
+        thp1 = (iTh-0.5)*dth1
+        rp1 = _orbitRadius(thp1,E,L,true)  # min value (left of rc)
+
+        halfperiod += dth1*cos(thp1)/abs(dpsiEffdr(rp1,L))
+
+        localOrbitChangeIso!(rp1,E,L,m_field,PlummerTable)
+
+        dE   += dth1*cos(thp1)/abs(dpsiEffdr(rp1,L))*PlummerTable.dE[] 
+        dE2  += dth1*cos(thp1)/abs(dpsiEffdr(rp1,L))*PlummerTable.dE2[] 
+        dL   += dth1*cos(thp1)/abs(dpsiEffdr(rp1,L))*PlummerTable.dL[]  
+        dL2  += dth1*cos(thp1)/abs(dpsiEffdr(rp1,L))*PlummerTable.dL2[]
+        dEdL += dth1*cos(thp1)/abs(dpsiEffdr(rp1,L))*PlummerTable.dEdL[]
+    end
+
+    for iTh=1:nbT2
+        thp2 = (iTh-0.5)*dth2
+        rp2 = _orbitRadius(thp2,E,L,false) # max value (right of rc)
+
+        halfperiod += dth2*cos(thp2)/abs(dpsiEffdr(rp2,L))
+        
+        localOrbitChangeIso!(rp2,E,L,m_field,PlummerTable)
+
+        dE   += dth1*cos(thp2)/abs(dpsiEffdr(rp2,L))*PlummerTable.dE[]  
+        dE2  += dth1*cos(thp2)/abs(dpsiEffdr(rp2,L))*PlummerTable.dE2[]  
+        dL   += dth1*cos(thp2)/abs(dpsiEffdr(rp2,L))*PlummerTable.dL[]  
+        dL2  += dth1*cos(thp2)/abs(dpsiEffdr(rp2,L))*PlummerTable.dL2[] 
+        dEdL += dth1*cos(thp2)/abs(dpsiEffdr(rp2,L))*PlummerTable.dEdL[] 
+    end
+    halfperiod *= sqrt(2)
+
+    dE   *= sqrt(2)
+    dE2  *= sqrt(2)
+    dL   *= sqrt(2)
+    dL2  *= sqrt(2)
+    dEdL *= sqrt(2)
+
+    for iR=1:nbr
+        rp = r1 + (iR-0.5)*dr
+
+        halfperiod += dr/sqrt(2*(psiEff(rp,L)-E))
+
+        localOrbitChangeIso!(rp,E,L,m_field,PlummerTable)
+
+        dE   += dr/sqrt(2*(psiEff(rp,L)-E))*PlummerTable.dE[] 
+        dE2  += dr/sqrt(2*(psiEff(rp,L)-E))*PlummerTable.dE2[] 
+        dL   += dr/sqrt(2*(psiEff(rp,L)-E))*PlummerTable.dL[] 
+        dL2  += dr/sqrt(2*(psiEff(rp,L)-E))*PlummerTable.dL2[] 
+        dEdL += dr/sqrt(2*(psiEff(rp,L)-E))*PlummerTable.dEdL[] 
+    end
+    dE   /= halfperiod
+    dE2  /= halfperiod
+    dL   /= halfperiod
+    dL2  /= halfperiod
+    dEdL /= halfperiod
+
+    PlummerTable.dE[]   = dE
+    PlummerTable.dE2[]  = dE2
+    PlummerTable.dL[]   = dL
+    PlummerTable.dL2[]  = dL2
+    PlummerTable.dEdL[] = dEdL
+
+    end
+end
