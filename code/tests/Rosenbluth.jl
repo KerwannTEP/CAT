f616d2d1ca77997712bf484fd4b73a93491f111a
KerwannTEP <kerwann_tep@yahoo.fr>
2021-01-12 18:11:25 +0100

    compare two integral forms

--- code/tests/Rosenbluth.jl
+++ code/tests/Rosenbluth.jl
@@ -33,18 +33,17 @@ function angMoment(r::Float64, vr::Float64, vt::Float64)
 end
 
 function normVelIso(r::Float64, E::Float64)
-    return psi(r) - (v^2)/2
+    return sqrt(2*(psi(r) - E))
 end
 
 function _vmax(r::Float64, vr::Float64, vt::Float64)
-    return vr+vt+sqrt(2*(vr*vt+psi(r)))
+    return (vr+vt) + sqrt(2*(vr*vt+psi(r)))
 end
 
-vmax = _vmax(r,vr,vt)
 MAXEVALS = 5000
 
 function DF(E::Float64)
-    if (E <= 0.0 || L <= 0.0) # If E or L are negative, the DF vanishes
+    if (E <= 0.0) # If E or L are negative, the DF vanishes
         return 0.0
     else # If E and L are positive
         return 3.0/(7.0*pi^3) * (2.0*E)^(7/2)
@@ -59,4 +58,21 @@ function _hIso(r::Float64, v::Float64)
     return 4*pi*(I1+I2)
 end
 
-println(_hIso(r,v))
+function Efield(r::Float64, vr::Float64, vt::Float64, vp::Float64, 
+                th::Float64, phi::Float64)
+    vfield_r = vr - vp*cos(th)
+    vfield_t = sqrt((vt-vp*sin(th)*cos(phi))^2 + (vp*sin(th)*sin(phi))^2)
+    return psi(r) - (vfield_r^2)/2 - (vfield_t^2)/2
+end
+
+function _hAni(r::Float64, vr::Float64, vt::Float64)
+    # x = (vp, th, phi)
+    vmax = _vmax(r,vr,vt)
+    int1 = (x->x[1]*sin(x[2])*DF(Efield(r,vr,vt,x[1],x[2],x[3])))
+    I = hcubature(int1,[0,0,0],[vmax,pi,2*pi],maxevals=MAXEVALS)[1]
+    return I
+end
+    
+
+println("Iso = ",_hIso(r,v))
+println("Ani = ",_hAni(r,vr,vt))
